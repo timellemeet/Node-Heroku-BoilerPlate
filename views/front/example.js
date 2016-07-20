be7cc5ad2c4954d80047592dbace1c0c70771343
timmetje <tim@ellemeet.net>
2016-07-20 16:26:56 +0200

    structure

---
+++ views/front/example.js
@@ -0,0 +1,12 @@
+var example = {
+    title : 'Hello, world!',
+        id : 'main',
+    links: [
+    { name : 'Google', url : 'http://google.com/' },
+    { name : 'Facebook', url : 'http://facebook.com/' },
+    { name : 'Twitter', url : 'http://twitter.com/' }
+],
+    upperHelper : function (string) {
+    return string.toUpperCase();
+}
+}
